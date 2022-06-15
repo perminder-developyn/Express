18d6ea0ce60a1866b8263bee6b17a8ec93a937d9
perminder-developyn <perminder.dhillon@developyn.com>
2022-06-15 13:14:33 +0100

    hello name API added

---
+++ HelloName/express.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
